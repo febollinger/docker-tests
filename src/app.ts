11ed8c843e24ca8ffe0b6d631c19c63d955f667f
Fernanda Bollinger <feernanda.bollinger@gmail.com>
2024-06-26 14:03:07 -0300

    settings and connection fixed

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { Application } from "express";
+
+const app: Application = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("tudo bem");
+});
+
+export default app
